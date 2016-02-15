f153ec25a2f98236875bde922d46bb5fee886713
edgardleal <edgardleal@gmail.com>
2016-02-15 14:49:57 -0300

    Improve configuration reader to read config file in user home

--- src/main/java/com/edgardleal/sqlshell/Main.java
+++ src/main/java/com/edgardleal/sqlshell/Main.java
@@ -5,10 +5,16 @@ import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import org.apache.commons.lang3.time.DurationFormatUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+  private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
   public static void main(String[] args) throws FileNotFoundException, IOException, SQLException {
 
+    long start = System.currentTimeMillis();
     StringBuilder builder = new StringBuilder();
 
     Config.current_connection = args[0];
@@ -22,7 +28,6 @@ public class Main {
       builder.append(args[i]).append(' ');
     }
 
-
     if (builder.length() > 0) {
       ConnectionFactory.getConnection();
       try (ResultSet resultSet = ConnectionFactory.getStatement(builder.toString()).executeQuery()) {
@@ -35,6 +40,8 @@ public class Main {
     }
 
     ConnectionFactory.close();
+
+    LOGGER.info(DurationFormatUtils.formatDurationISO(System.currentTimeMillis() - start));
   }
 
 }
